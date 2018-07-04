78ac662ef993b755fa49f924a00416038be36fab
Andres Bonilla <andresbonilla@gmail.com>
2018-07-03 20:50:32 -0400

    Removes console log

--- signalCacheCleaner.js
+++ signalCacheCleaner.js
@@ -5,9 +5,5 @@ const TTL = 1000 * 30
 const DELAY = 1000 * 5
 
 setInterval(() => {
-  redisClient.zremrangebyscore(['SIGNAL_CACHE', '-inf', (Date.now() - TTL)], () => {
-    redisClient.zcount(['SIGNAL_CACHE', '-inf', '+inf'], (err, count) => {
-      console.log('cache size: ' + count);
-    })
-  })
+  redisClient.zremrangebyscore(['SIGNAL_CACHE', '-inf', (Date.now() - TTL)])
 }, DELAY)
