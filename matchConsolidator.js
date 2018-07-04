c687d282cfe884b191947e435a36becd327ceea0
Andres Bonilla <andresbonilla@gmail.com>
2018-07-03 21:22:56 -0400

    Adds missing err

--- matchConsolidator.js
+++ matchConsolidator.js
@@ -97,7 +97,7 @@ jobs.process('match-segment', CONCURRENT_JOBS, (job, done) => {
         channels: 1
       })
 
-      redisClient.zrange(['SIGNAL_CACHE', paddedStartTime.toString(), paddedEndTime.toString()], (chunkStrings) => {
+      redisClient.zrange(['SIGNAL_CACHE', paddedStartTime.toString(), paddedEndTime.toString()], (err, chunkStrings) => {
         chunkStrings.forEach((chunkString) => {
           ws.write(Buffer.from(chunkString, 'utf8'))
         })
